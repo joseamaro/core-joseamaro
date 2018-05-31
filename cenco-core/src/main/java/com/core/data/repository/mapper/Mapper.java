c52e23197924cc3178e60c68f80a89f672d32006
joseamaro <joseamaro905@gmail.com>
2018-05-31 17:00:26 -0400

    se modifica el core mapper

--- cenco-core/src/main/java/com/core/data/repository/mapper/Mapper.java
+++ cenco-core/src/main/java/com/core/data/repository/mapper/Mapper.java
@@ -2,7 +2,9 @@ package com.core.data.repository.mapper;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by jhonnybarrios on 10/23/17.
@@ -26,6 +28,23 @@ public abstract class Mapper<T1, T2> {
         ArrayList<T1> returnValues = new ArrayList<>();
         for (T2 value : values) {
             returnValues.add(reverseMap(value));
+        }
+        return returnValues;
+    }
+
+    public Map<String,T2>  map(Map<String,T1> values){
+
+        Map<String,T2> returnValues = new HashMap<>();
+        for (Map.Entry<String, T1> entry : values.entrySet()) {
+            returnValues.put(entry.getKey(),map(entry.getValue()));
+        }
+        return returnValues;
+    }
+
+    public Map<String,T1> reverseMap(Map<String,T2> values) {
+        Map<String,T1> returnValues = new HashMap<>();
+        for (Map.Entry<String, T2> entry : values.entrySet()) {
+            returnValues.put(entry.getKey(),reverseMap(entry.getValue()));
         }
         return returnValues;
     }
